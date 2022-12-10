65edebf6753df194cbcda8bbd918f64614369769
David Merrick <david@david-merrick.com>
2022-12-09 21:52:00 -0800

    Gradle automation and day 9 boilerplate

---
+++ src/test/kotlin/io/github/davidmerrick/aoc2022/day9/Day9Test.kt
@@ -0,0 +1,11 @@
+package io.github.davidmerrick.aoc2022.day9
+
+import org.junit.jupiter.api.Test
+
+class Day9Test {
+
+    @Test
+    fun `Part 1 example`() {
+        TODO()
+    }
+}
---
+++ src/test/kotlin/io/github/davidmerrick/aoc2022/template/DayTest.kt
@@ -0,0 +1,11 @@
+package io.github.davidmerrick.aoc2022.template
+
+import org.junit.jupiter.api.Test
+
+class DayTest {
+
+    @Test
+    fun `Part 1 example`() {
+        TODO()
+    }
+}
