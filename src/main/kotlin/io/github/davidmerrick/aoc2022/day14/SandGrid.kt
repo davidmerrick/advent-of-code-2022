6b9eb2718734d5ce4d0552ef8f6daded29192f42
David Merrick <david@david-merrick.com>
2022-12-14 17:19:53 -0800

    Started SandGrid class

---
+++ src/main/kotlin/io/github/davidmerrick/aoc2022/day14/SandGrid.kt
@@ -0,0 +1,13 @@
+package io.github.davidmerrick.aoc2022.day14
+
+import com.google.common.collect.HashBasedTable
+
+class SandGrid(private val table: HashBasedTable<Int, Int, Boolean>) {
+
+
+    companion object {
+        fun of(lines: List<String>): SandGrid {
+            TODO()
+        }
+    }
+}
