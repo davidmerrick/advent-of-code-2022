e9e9b4adea70e0a993a7ae279738a2c66ae55b06
David Merrick <david@david-merrick.com>
2022-12-15 13:43:04 -0800

    Work on parsing table

--- src/main/kotlin/io/github/davidmerrick/aoc2022/day14/SandGrid.kt
+++ src/main/kotlin/io/github/davidmerrick/aoc2022/day14/SandGrid.kt
@@ -1,12 +1,22 @@
 package io.github.davidmerrick.aoc2022.day14
 
 import com.google.common.collect.HashBasedTable
+import io.github.davidmerrick.aoc.coordinates.Range
+import io.github.davidmerrick.aoc.guava.put
 
 class SandGrid(private val table: HashBasedTable<Int, Int, Boolean>) {
 
 
     companion object {
         fun of(lines: List<String>): SandGrid {
+            val table = HashBasedTable.create<Int, Int, Boolean>()
+            lines.flatMap { parseRanges(it) }
+                .flatMap { it.positions }
+                .forEach { table.put(it, true) }
+            return SandGrid(table)
+        }
+
+        private fun parseRanges(line: String): List<Range> {
             TODO()
         }
     }
